1131229c9e4a6577b64168af60abb5bea02fbe10
Arbin-1203 <122951773+Arbin-1203@users.noreply.github.com>
2024-01-23 21:40:16 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
+
